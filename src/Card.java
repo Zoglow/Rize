f67ff0ef1fefd1920dcc1a53c48495d948de2237
Zoe Olson <zoglow2311@gmail.com>
2022-10-23 23:25:59 -0400

    create card class

---
+++ src/Card.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Card {
+    private String side1;
+    private String side2;
+    private ArrayList<String> notes;
+
+    /**
+     * Constructs new card with two different sides
+     * @param side1 represents the question side
+     * @param side2 represents the answer side
+     */
+    public Card(String side1, String side2, ArrayList<String> notes) {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    public String getSide1() {
+        return side1;
+    }
+
+    public void setSide1(String side1) {
+        this.side1 = side1;
+    }
+
+    public String getSide2() {
+        return side2;
+    }
+
+    public void setSide2(String side2) {
+        this.side2 = side2;
+    }
+
+    public ArrayList<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(ArrayList<String> notes) {
+        this.notes = notes;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! It's my first pah-ty fathah");
+    }
+}
