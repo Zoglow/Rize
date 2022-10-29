d1d46d86b111a01b324346fdad200e3a341592b4
Zoe Olson <zoglow2311@gmail.com>
2022-10-29 00:27:32 -0400

    Add import cards from csv file and display in loop

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,68 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static ArrayList<Card> cards = new ArrayList<>();
+    static ArrayList<Card> discardedCards = new ArrayList<>();
+
+    /**
+     * Populates card stack with csv data from file
+     */
+    public static void importCards() {
+        String front;
+        String back;
+
+        try {
+            File cardFile = new File("rizeCards.csv");
+            Scanner readCards = new Scanner(cardFile);
+            readCards.useDelimiter(",");
+
+            while (readCards.hasNextLine()) {
+                front = readCards.next();
+                back = readCards.next();
+                readCards.nextLine();
+
+                cards.add(new Card(front, back));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("There is no file with that name.\n");
+        } catch (InputMismatchException m) {
+            System.out.println("The file did not contain valid data.");
+        }
+    }
+
     public static void main(String[] args) {
-
-        Card testCard = new Card("안녕", "Hi");
+        importCards();
         Scanner scan = new Scanner(System.in);
-        boolean keepStudying = true;
         boolean flipped = false;
-        System.out.println(testCard.getSide1());
 
-        while (keepStudying) {
-            System.out.println("Flip card: F, Discard: D, Restudy: R");
-            char input = scan.next().charAt(0);
+        for (int i = 0; i < cards.size(); i++) {
 
-            if ((input == 'F') || (input == 'f')) {
-                flipped = (!flipped);
-                String text = (flipped) ? testCard.getSide2() : testCard.getSide1();
-                System.out.println(text);
-            } else if ((input == 'D')||(input == 'd')) {
-                keepStudying = false;
-                
+            boolean keepStudying = true;
+
+            Card curr = cards.get(i);
+            System.out.println(curr.getSide1());
+
+            while (keepStudying) {
+                System.out.println("Flip card: F, Discard: D, Restudy: R");
+                char input = scan.next().charAt(0);
+
+                if ((input == 'F') || (input == 'f')) {
+                    flipped = (!flipped);
+                    String text = (flipped) ? curr.getSide2() : curr.getSide1();
+                    System.out.println(text);
+                } else if ((input == 'D')||(input == 'd')) {
+                    discardedCards.add(curr);
+                    cards.remove(curr);
+                    i--;
+                    keepStudying = false;
+                }
             }
         }
+
+
+
 
     }
 }
