50895b3431de46b08fc06a394868107e7057104d
Zoe Olson <zoglow2311@gmail.com>
2022-10-29 14:11:23 -0400

    Add MainView gui (no code yet)

---
+++ src/MainView.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainView {
+    private JPanel MainPanel;
+    private JButton discardButton;
+    private JButton restudyButton;
+    private JTextField addNoteField;
+    private JLabel cardText;
+    private JLabel addNoteText;
+    private JScrollPane notesListScroll;
+    private JTextArea notesTextArea;
+}
